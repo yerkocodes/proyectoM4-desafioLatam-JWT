1a3016615f0a2fa2a1797af70cf1c946f3621123
Reinaldo Carocca <reinaldo_carocca@yahoo.es>
2021-08-22 19:06:30 -0400

    se añadieron comentarios explicativos

--- public/desafio-instafake/assets/scripts.js
+++ public/desafio-instafake/assets/scripts.js
@@ -1,26 +1,27 @@
-//Test Reinaldo
-// Form capture
+// Form captura
 const form = document.getElementById('js-form');
+//Arreglo donde se añadiran los Posts
 let posts = [];
+//Captura del boton para mostrar más Posts
 const btnShowMore = document.getElementById('btnShowMore');
 
-// Event Listener to form submit
+// Event Listener to form submit login
 form.addEventListener('submit', async (e) => {
 	e.preventDefault();
 
-	const email = document.getElementById('js-input-email').value;
-	const pass = document.getElementById('js-input-password').value;
+	const email = document.getElementById('js-input-email').value; //Constante donde se almacena Imputs de Email
+	const pass = document.getElementById('js-input-password').value; //Constante donde se almacena Imputs de Paswword
 
-	const JWT = await login(email, pass);
-	posts = await getPosts(JWT)
+	const JWT = await login(email, pass); //Conatante donde se almacena el token del usuario
+	posts = await getPosts(JWT) //Al arreglo posts se añadira la data obtenida por la function getPosts()
 
 		if(posts.length !== 0){
-			addPosts(posts)
-			btnShowMore.setAttribute('style','display:block;')
+			addPosts(posts)  //Si la cantidad elementos del array !=0 se ejecuta functin addPosts y se pasa parametro el arreglo
+			btnShowMore.setAttribute('style','display:block;') //Atributo que muestra boton Mostrar mas al iniciar seccion
 		}
 })
 
-// Funcion que Verifica y obtiene el TOKEN
+// Funcion Asincrona que Verifica y obtiene el TOKEN del usuario y constraseña ingresados
 const login = async (email, password) => {
 	try {
 		const response = await fetch(`http://localhost:3000/api/login`, {
@@ -30,24 +31,24 @@ const login = async (email, password) => {
 				"password" : password
 			})
 		});
-		const { token } = await response.json();
-		localStorage.setItem('jwt-token', token)
+		const { token } = await response.json(); //Almacena en una constante token la respuesta en formato JSON
+		localStorage.setItem('jwt-token', token) //Setea en localStorage el nombre y token
 		return token
 	} catch(err) {
 		console.error(`Error: ${err}`)
 	}
 }
 
-// Funcion que retorna un arreglo con los posts 
+// Funcion Asincrona que retorna un arreglo con los primeros 100 posts 
 const getPosts = async (jwt) => {
 	try {
-		const res = await fetch(`http://localhost:3000/api/photos`, {
+		const res = await fetch(`http://localhost:3000/api/photos`, { 
 			method:'GET',
 			headers: {
 				Authorization: `Bearer ${jwt}`
 			}
 		})
-		const { data } = await res.json(); // $$$ ? {}
+		const { data } = await res.json();
 		//console.log(data)
 		return data;
 	} catch(err) {
@@ -55,22 +56,16 @@ const getPosts = async (jwt) => {
 	}
 };
 
+//Function Asincrona que agrega los siguientes 100 posts al arreglo actual
 const getMorePosts = async (jwt, page) => {
 	try {
 		const res = await fetch(`http://localhost:3000/api/photos?page=${page}`, {
 			method:'GET',
 			headers: {
-				Authorization: `Bearer ${jwt}`
+				Authorization: `Bearer ${jwt}` //Verificación de usuario para obtener datos 
 			},
-			//params: { $$$
-			//	page: page
-			//}
 		})
 		const { data } = await res.json(); // $$$ ? {}
-		//console.log(data)
-		//if(data){
-		//	addPosts(data)
-		//}
 		return data;
 	} catch(err) {
 		console.error(`Error ${err}`)
@@ -78,9 +73,9 @@ const getMorePosts = async (jwt, page) => {
 };
 
 
-// Funcion que manipula y anade los posts en el DOM
-	const instaFotos = document.getElementById('instaFotos')
+const instaFotos = document.getElementById('instaFotos') //Captura de seccion html donde se añadiran los Posts
 
+// Funcion que manipula y añade los posts en el DOM
 const addPosts = (posts) => {
 
 	let template ='';
@@ -91,9 +86,9 @@ const addPosts = (posts) => {
 		</div>
 	`;
 
-	instaFotos.innerHTML = titlePage;
+	instaFotos.innerHTML = titlePage; //Añade a la sección instafotos el contenido de la variable titlePage
 
-	posts.forEach((element) => {
+	posts.forEach((element) => { //Se recorre el arreglo Posts y se añade cada posts 
 		template += `
 			<div class="card w-50 my-5 mx-auto">
 				<img src="${element.download_url}" class="card-img-top" alt="...">
@@ -103,22 +98,22 @@ const addPosts = (posts) => {
 			</div>
 		`
 	})
-	instaFotos.innerHTML += template;
+
+	instaFotos.innerHTML += template; //Añade a la sección instafotos el contenido de la variable template(posts)
 
 	// Esconder formulario
 	form.setAttribute('style','display:none;')
 }
-
-// Show more photos Btn
-
+//Se inicializa la variable contador en 2
 let contador = 2;
 
+// Funcion Asisncrona que muestra 100 posts más
  const showMorePhotos = async () => {
-	const JWT = localStorage.getItem("jwt-token")
-	const data = await getMorePosts(JWT, contador);
+	const JWT = localStorage.getItem("jwt-token") //Se alamacena en una constante el token almacenado con nombre "jwt-token"
+	const data = await getMorePosts(JWT, contador); //Se almacena en una constante la data obtenida con la function getMorePosts()
 
 	let template= ''
-	data.forEach((element) => {
+	data.forEach((element) => { //Se recorre la data y genera un template con los elementos para los posts
 		template += `
 			<div class="card w-50 my-5 mx-auto">
 				<img src="${element.download_url}" class="card-img-top" alt="...">
@@ -128,8 +123,9 @@ let contador = 2;
 			</div>
 		`
 	})
-	instaFotos.innerHTML += template;
-	contador++
+
+	instaFotos.innerHTML += template; //Añade a la sección instafotos cada posts obtenido con el forEach
+	contador++ //A la variable contador se le suma 1
 	//console.log(posts)
 }
 
@@ -140,7 +136,7 @@ const logout = () => {
 	location.reload(); // Recarga la pagina al cerrar sesion
 }
 
-// IIFE validate if exists token in localStorge, to hide login and load photos
+// IIFE valida si existe token en el localStorge, para ocultar login y cargar las fotos
 ( async () => {
 	if ( localStorage.getItem('jwt-token') ) {
 		form.setAttribute('style','display:none;');
