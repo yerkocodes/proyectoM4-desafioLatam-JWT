34bb620bd02e36d4576a1b554eb7f424d4eac71a
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-08-24 12:50:49 -0400

    Modal data capture ready

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,7 +1,16 @@
 const api = 'http://localhost:3000/api/total';
 
 const login = document.getElementById('login');
+const email = document.getElementById('js-input-email').value;
+const pass = document.getElementById('js-input-password').value;
+const modalForm = document.getElementById('js-form');
 
-login.addEventListener('click', () => {
+login.addEventListener('click', (e) => {
+   e.preventDefault();
    console.log('Bien hecho')
+});
+
+modalForm.addEventListener('submit', (e) => {
+   e.preventDefault();
+   console.log(`${email} y ${pass}`);
 });
