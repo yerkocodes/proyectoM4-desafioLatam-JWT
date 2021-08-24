71e4f064526c6e7e0ff9df6c5a6c6290df4efb7a
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-08-24 19:45:19 -0400

    Show cases Covid in Chart ready

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -8,161 +8,145 @@ const modalForm = document.getElementById('js-form');
 // BTN 'INICIAR SESION'
 login.addEventListener('click', (e) => {
    e.preventDefault();
-   console.log('Bien hecho')
 });
 
 // LOGIN FROM
-modalForm.addEventListener('submit', (e) => {
+modalForm.addEventListener('submit', async (e) => {
+   const contenido = document.getElementById('contenido');
    const email = document.getElementById('js-input-email').value;
    const pass = document.getElementById('js-input-password').value;
-
    e.preventDefault();
-   getTokenUser(email, pass);
-
-   //console.log(`${email} y ${pass}`);
+   const jwt = await getTokenUser(email, pass);
+   const dataUser = await getData(jwt);
+   console.log(dataUser);
+   const showChart = mostrarGrafico(dataUser);
+   contenido.innerHTML = showChart;
 });
 
 // VERIFY USER AND GET TOKEN
 const getTokenUser = async (email, password) => {
    try {
-		const response = await fetch(`${api}/login`, {
-			method:'POST',
-			body: JSON.stringify({
-				"email" : email,
-				"password" : password
-			})
-		});
-		const { token } = await response.json();
-
+      const response = await fetch(`${api}/login`, {
+         method:'POST',
+         body: JSON.stringify({
+            "email" : email,
+            "password" : password
+         })
+      });
+      const { token } = await response.json();
       if ( token !== undefined ) {
-	   	localStorage.setItem('jwt-token', token)
+         localStorage.setItem('jwt-token', token)
          successLogin();
-         console.log('Si existe el token')
-         console.log(token);
-	   	return token
+         //console.log(token);
+         return token
       }
-	} catch(err) {
-		console.error(`Error: ${err}`)
-	}
+   } catch(err) {
+      console.error(`Error: ${err}`)
+   }
 }
 
 const successLogin = () => {
    const formButtons = document.getElementById('form-buttons');
-	let template = `
+   let template = `
       <button type="button" class="btn btn-secondary my-4" data-dismiss="modal">Close</button>
       <p class="text-success">Sesion iniciada correctamente.</p>
    `;
-
    formButtons.innerHTML = template;
 };
 
 // GET DATA FUNCTION
-const getData = () => {
-   
+const getData = async (jwt) => {
+   try{
+      const response = await fetch(`${api}/total`, {
+         method: 'GET',
+         headers: {
+            Authorization: `Bearer ${jwt}`,
+         }
+      });
+      const { data } = await response.json();
+      //console.log(data);
+      return data;
+   } catch(err) {
+      console.error(err);
+   }
 };
 
 // CHART
-window.onload = function () {
+const mostrarGrafico = (data) => {
 
-   var chart = new CanvasJS.Chart("chartContainer", {
-      animationEnabled: true,
-      title:{
-         text: "Paises con Covid19"
-      },
-      legend: {
-         cursor:"pointer",
-         itemclick : toggleDataSeries
-      },
-      toolTip: {
-         shared: true,
-         content: toolTipFormatter
-      },
-      data: [{
-         type: "column",
-         showInLegend: true,
-         name: "Casos Activos",
-         color: "red",
-         dataPoints: [
-            { y: 243, label: "Italy" },
-            { y: 236, label: "China" },
-            { y: 243, label: "France" },
-            { y: 273, label: "Great Britain" },
-            { y: 269, label: "Germany" },
-            { y: 196, label: "Russia" },
-            { y: 1118, label: "USA" }
+      let activos = [];
+      let recuperados = [];
+      let muertos = [];
+      let confirmados = [];
+      
+      let limit = 500000;
+
+      const dataConfirmed = data.filter((element)=>{
+         return element.confirmed >= limit;
+      });
+      dataConfirmed.forEach((element)=>{
+         confirmados.push({ y: element.confirmed, label: element.location });
+      })
+
+      const dataDeaths = data.filter((element)=>{
+         return element.deaths >= limit;
+      });
+      dataDeaths.forEach((element)=>{
+         muertos.push({ y: element.deaths, label: element.location });
+      })
+      console.log(confirmados);
+
+      var chart = new CanvasJS.Chart("chartContainer", {
+
+         animationEnabled: true,
+         title:{
+            text: "Casos Covid19"
+         },
+         toolTip: {
+            shared: true
+         },
+         legend: {
+            cursor:"pointer",
+         },
+         axisX:{
+           labelAngle: 50,
+           interval: 1,
+         },
+         axisY:{
+            title: "",
+            gridThickness: 1
+         },
+         data: [{
+            type: "column",
+            name: "Confirmados",
+            legendText: "Confirmados",
+            showInLegend: true, // Banderines
+            dataPoints:confirmados 
+         },
+            {
+               type: "column",	
+               name: "Recuperados",
+               legendText: "Recuperados",
+               axisYType: "secondary",
+               showInLegend: true,
+               dataPoints:recuperados
+            },
+            {
+               type: "column",	
+               name: "Muertos",
+               legendText: "Muertos",
+               axisYType: "secondary",
+               showInLegend: true,
+               dataPoints:muertos
+            },{
+               type: "column",	
+               name: "Activos",
+               legendText: "Activos",
+               axisYType: "secondary",
+               showInLegend: true,
+               dataPoints:activos
+            }
          ]
-      },
-         {
-            type: "column",
-            showInLegend: true,
-            name: "Casos confirmados",
-            color: "yellow",
-            dataPoints: [
-               { y: 212, label: "Italy" },
-               { y: 186, label: "China" },
-               { y: 272, label: "France" },
-               { y: 299, label: "Great Britain" },
-               { y: 270, label: "Germany" },
-               { y: 165, label: "Russia" },
-               { y: 896, label: "USA" }
-            ]
-         },
-         {
-            type: "column",
-            showInLegend: true,
-            name: "Casos muertos",
-            color: "gray",
-            dataPoints: [
-               { y: 236, label: "Italy" },
-               { y: 172, label: "China" },
-               { y: 309, label: "France" },
-               { y: 302, label: "Great Britain" },
-               { y: 285, label: "Germany" },
-               { y: 188, label: "Russia" },
-               { y: 788, label: "USA" }
-            ]
-         },
-         {
-            type: "column",
-            showInLegend: true,
-            name: "Casos recuperados",
-            color: "turquoise",
-            dataPoints: [
-               { y: 236, label: "Italy" },
-               { y: 172, label: "China" },
-               { y: 309, label: "France" },
-               { y: 302, label: "Great Britain" },
-               { y: 285, label: "Germany" },
-               { y: 188, label: "Russia" },
-               { y: 788, label: "USA" }
-            ]
-         }]
-   });
-   chart.render();
-
-   function toolTipFormatter(e) {
-      var str = "";
-      var total = 0 ;
-      var str3;
-      var str2 ;
-      for (var i = 0; i < e.entries.length; i++){
-         var str1 = "<span style= \"color:"+e.entries[i].dataSeries.color + "\">" + e.entries[i].dataSeries.name + "</span>: <strong>"+  e.entries[i].dataPoint.y + "</strong> <br/>" ;
-         total = e.entries[i].dataPoint.y + total;
-         str = str.concat(str1);
-      }
-      str2 = "<strong>" + e.entries[0].dataPoint.label + "</strong> <br/>";
-      str3 = "<span style = \"color:Tomato\">Total: </span><strong>" + total + "</strong><br/>";
-      return (str2.concat(str)).concat(str3);
-   }
-
-   function toggleDataSeries(e) {
-      if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-         e.dataSeries.visible = false;
-      }
-      else {
-         e.dataSeries.visible = true;
-      }
+      });
       chart.render();
-   }
-
 }
