f8a0d00849cc25cd934011982842dc5e00b46f1c
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-09-02 18:28:42 -0400

    Project 'Situacion Mundial, hito 1' ready

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,163 +1,168 @@
-// API
-const api = 'http://localhost:3000/api';
+// API's
+const API = 'http://localhost:3000/api';
+const APITOTAL = 'http://localhost:3000/api/total';
+
+window.onload = async () => {
+   try {
+        const response = await fetch(`${APITOTAL}`)
+        const {data} = await response.json()
+        mostrarGrafico(data)
+        mostrarTabla(data);
+    }catch(err){
+        console.error(`Error: ${err}`)
+    }
+}
 
 // HTML CAPTURE 
-const login = document.getElementById('login');
+const navbarLogin = document.getElementById('login');
 const modalForm = document.getElementById('js-form');
 
 // BTN 'INICIAR SESION'
-login.addEventListener('click', (e) => {
-   e.preventDefault();
+navbarLogin.addEventListener('click', (e) => {
+  e.preventDefault();
 });
 
 // LOGIN FROM
 modalForm.addEventListener('submit', async (e) => {
-   const contenido = document.getElementById('contenido');
+  const contenido = document.getElementById('contenido');
 
-   const email = document.getElementById('js-input-email').value;
-   const pass = document.getElementById('js-input-password').value;
+  const email = document.getElementById('js-input-email').value;
+  const pass = document.getElementById('js-input-password').value;
 
-   e.preventDefault();
+  e.preventDefault();
 
-   const jwt = await getTokenUser(email, pass);
+  const jwt = await getTokenUser(email, pass);
 
-   const dataUser = await getData(jwt);
+  const dataUser = await getData(jwt);
 
-   //console.log(dataUser);
-   
-   const showChart = mostrarGrafico(dataUser);
-
-   //contenido.innerHTML = showChart;
-
-   mostrarTabla(dataUser);
 });
 
 // VERIFY USER AND GET TOKEN
 const getTokenUser = async (email, password) => {
-   try {
-      const response = await fetch(`${api}/login`, {
-         method:'POST',
-         body: JSON.stringify({
-            "email" : email,
-            "password" : password
-         })
-      });
-      const { token } = await response.json();
-      if ( token !== undefined ) {
-         localStorage.setItem('jwt-token', token)
-         successLogin();
-         //console.log(token);
-         return token
-      }
-   } catch(err) {
-      console.error(`Error: ${err}`)
-   }
+  try {
+    const response = await fetch(`${API}/login`, {
+      method:'POST',
+      body: JSON.stringify({
+        "email" : email,
+        "password" : password
+      })
+    });
+    const { token } = await response.json();
+    if ( token !== undefined ) {
+      localStorage.setItem('jwt-token', token)
+      successLogin();
+      //console.log(token);
+      return token
+    }
+  } catch(err) {
+    console.error(`Error: ${err}`)
+  }
 }
 
 const successLogin = () => {
-   const formButtons = document.getElementById('form-buttons');
-   let template = `
+  const formButtons = document.getElementById('form-buttons');
+  let template = `
       <button type="button" class="btn btn-secondary my-4" data-dismiss="modal">Close</button>
       <p class="text-success">Sesion iniciada correctamente.</p>
    `;
-   formButtons.innerHTML = template;
+  formButtons.innerHTML = template;
 };
 
 // GET DATA FUNCTION
 const getData = async (jwt) => {
-   try{
-      const response = await fetch(`${api}/total`, {
-         method: 'GET',
-         headers: {
-            Authorization: `Bearer ${jwt}`,
-         }
-      });
-      const { data } = await response.json();
-      //console.log(data);
-      return data;
-   } catch(err) {
-      console.error(err);
-   }
+  try{
+    const response = await fetch(`${API}/total`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      }
+    });
+    const { data } = await response.json();
+    //console.log(data);
+    return data;
+  } catch(err) {
+    console.error(err);
+  }
 };
 
 // CHART
 const mostrarGrafico = (data) => {
 
-   let activos = [];
-   let recuperados = [];
-   let muertos = [];
-   let confirmados = [];
+  let activos = [];
+  let recuperados = [];
+  let muertos = [];
+  let confirmados = [];
 
-   let limit = 500000;
+  let limit = 500000;
 
-   const dataConfirmed = data.filter((element)=>{
-      return element.confirmed >= limit;
-   });
-   dataConfirmed.forEach((element)=>{
-      confirmados.push({ y: element.confirmed, label: element.location });
-   })
+  const dataConfirmed = data.filter((element)=>{
+    return element.confirmed >= limit;
+  });
+  dataConfirmed.forEach((element)=>{
+    confirmados.push({ y: element.confirmed, label: element.location });
+  })
 
-   const dataDeaths = data.filter((element)=>{
-      return element.deaths >= limit;
-   });
-   dataDeaths.forEach((element)=>{
-      muertos.push({ y: element.deaths, label: element.location });
-   })
-   console.log(confirmados);
+  const dataDeaths = data.filter((element)=>{
+    return element.deaths >= limit;
+  });
+  dataDeaths.forEach((element)=>{
+    muertos.push({ y: element.deaths, label: element.location });
+  })
+  console.log(confirmados);
 
-   var chart = new CanvasJS.Chart("chartContainer", {
+  var chart = new CanvasJS.Chart("chartContainer", {
 
-      animationEnabled: false,
-      title:{
-         text: "Casos Covid19"
+    animationEnabled: false,
+    title:{
+      text: "Casos Covid19"
+    },
+    toolTip: {
+      shared: true
+    },
+    legend: {
+      cursor:"pointer",
+    },
+    axisX:{
+      labelAngle: 50,
+      interval: 1,
+    },
+    axisY:{
+      title: "",
+      gridThickness: 1
+    },
+    data: [{
+      type: "column",
+      name: "Confirmados",
+      legendText: "Confirmados",
+      showInLegend: true, // Banderines
+      dataPoints:confirmados 
+    },
+      {
+        type: "column",	
+        name: "Recuperados",
+        legendText: "Recuperados",
+        axisYType: "secondary",
+        showInLegend: true,
+        dataPoints:recuperados
       },
-      toolTip: {
-         shared: true
-      },
-      legend: {
-         cursor:"pointer",
-      },
-      axisX:{
-         labelAngle: 50,
-         interval: 1,
-      },
-      axisY:{
-         title: "",
-         gridThickness: 1
-      },
-      data: [{
-         type: "column",
-         name: "Confirmados",
-         legendText: "Confirmados",
-         showInLegend: true, // Banderines
-         dataPoints:confirmados 
-      },
-         {
-            type: "column",	
-            name: "Recuperados",
-            legendText: "Recuperados",
-            axisYType: "secondary",
-            showInLegend: true,
-            dataPoints:recuperados
-         },
-         {
-            type: "column",	
-            name: "Muertos",
-            legendText: "Muertos",
-            axisYType: "secondary",
-            showInLegend: true,
-            dataPoints:muertos
-         },{
-            type: "column",	
-            name: "Activos",
-            legendText: "Activos",
-            axisYType: "secondary",
-            showInLegend: true,
-            dataPoints:activos
-         }
-      ]
-   });
-   chart.render();
+      {
+        type: "column",	
+        name: "Muertos",
+        legendText: "Muertos",
+        axisYType: "secondary",
+        showInLegend: true,
+        dataPoints:muertos
+      },{
+        type: "column",	
+        name: "Activos",
+        legendText: "Activos",
+        axisYType: "secondary",
+        showInLegend: true,
+        dataPoints:activos
+      }
+    ]
+  });
+  chart.render();
 }
 
 // FUNCION MOSTRAR TABLA
@@ -168,7 +173,7 @@ const tbody = document.getElementById('tbody');
 
 
 const mostrarTabla = (data) => {
-   const theadTemplate = `
+  const theadTemplate = `
               <tr>
                 <th scope="col">Nro</th>
                 <th scope="col">Pais</th>
@@ -179,12 +184,12 @@ const mostrarTabla = (data) => {
                 <th scope="col">Detalle</th>
               </tr>
    `;
-   thead.innerHTML = theadTemplate;
+  thead.innerHTML = theadTemplate;
 
-   let tbodyTemplate = '';
+  let tbodyTemplate = '';
 
-   data.forEach((element, index)=>{
-      let tbodyTemplate = `
+  data.forEach((element, index)=>{
+    let tbodyTemplate = `
               <tr>
                 <th scope="row">${index+1}</th>
                 <td>${element.location}</td>
@@ -192,11 +197,11 @@ const mostrarTabla = (data) => {
                 <td>${element.deaths}</td>
                 <td>${element.recovered}</td>
                 <td>${element.confirmed}</td>
-                <td><button onclick="funcion('${element.location}')" data-toggle="modal" data-target="#detalleModal" class="btn btn-primary">Ver Detalle</button></td>
+                <td><button onclick="countryDetails('${element.location}')" data-toggle="modal" data-target="#detalleModal" class="btn btn-primary">Ver Detalle</button></td>
               </tr>
       `;
-      tbody.innerHTML += tbodyTemplate;
-   })
+    tbody.innerHTML += tbodyTemplate;
+  })
 }
 
 
@@ -204,69 +209,66 @@ const mostrarTabla = (data) => {
 
 // Insertar data al grafico
 const modalDetalle = async (country) => {
-   try{
-      const data = await getDataCountry(country);
-   } catch (err){
-      console.error(err)
-   }
+  try{
+    const data = await getDataCountry(country);
+  } catch (err){
+    console.error(err)
+  }
 }
 
 // obtener la data de la location
 const getDataCountry = async (location) => {
-   try{
-      const response = await fetch(`http://localhost:3000/api/countries/${location}`);
-      const { data } = await response.json();
-      //console.log(data);
-      return data;
-   } catch(err) {
-      console.error(err);
-   }
+  try{
+    const response = await fetch(`${API}/countries/${location}`);
+    const { data } = await response.json();
+    //console.log(data);
+    return data;
+  } catch(err) {
+    console.error(err);
+  }
 };
 
 // Grafico a mostrar
 const mostrarGraficoDetalle = (data) => {
 
-   var chart = new CanvasJS.Chart("CHART", {
+  var chart = new CanvasJS.Chart("CHART", {
 
-      animationEnabled: true,
-      title:{
-         text: `Detalle ${data.location}`
-      },
-      axisY:{
-         title: "",
-      },
-      data: [{
-         type: "column",
-         indexLabelPlacement: "outside",
-         indexLabel: "{y}",
-         indexLabelOrientation: "horizontal",
-         dataPoints: [
-            { y: data.confirmed, label: "Confirmados"},
-            { y: data.deaths, label: "Muertos"},
-            { y: data.recovered, label: "Recuperados"},
-            { y: data.active, label: "Activos"}
-         ],
-      }
-      ]
-   });
-   chart.render();
+    animationEnabled: true,
+    title:{
+      text: `Detalle ${data.location}`
+    },
+    axisY:{
+      title: "",
+    },
+    data: [{
+      type: "column",
+      indexLabelPlacement: "outside",
+      indexLabel: "{y}",
+      indexLabelOrientation: "horizontal",
+      dataPoints: [
+        { y: data.confirmed, label: "Confirmados"},
+        { y: data.deaths, label: "Muertos"},
+        { y: data.recovered, label: "Recuperados"},
+        { y: data.active, label: "Activos"}
+      ],
+    }
+    ]
+  });
+  chart.render();
 }
 
 const idGrafico = document.getElementById('grafico');
 
-const funcion = async (param) => {
-   try{
-
-   console.log(`Funcionando ${param}`)
-
-   let data = await getDataCountry(`${param}`)
-
-   console.log(data);
-
-   mostrarGraficoDetalle(data);
-   
-   } catch (err) {
-      console.error(err);
-   }
+const countryDetails = async (param) => {
+  try{
+//    param.split(' ').join('_');
+//    console.log(param);
+//    console.log(`Funcionando ${param}`)
+    let data = await getDataCountry(`${param}`)
+//    console.log(data);
+    mostrarGraficoDetalle(data);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
